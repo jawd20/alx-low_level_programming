6883254d6113da212f8578355115746ca8c18a56
Jolomi Atsetse <jawd20@gmail.com>
2023-12-17 13:11:45 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: 1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdio.h);
+	return (1);
+}
