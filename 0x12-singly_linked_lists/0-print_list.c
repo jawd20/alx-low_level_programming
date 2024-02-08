a414f55d3832ab255c0cf77c3db1b65ee26b058d
jawd20 <jawd20@gmail.com>
2024-02-08 07:36:02 -0800

    help me work

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -21,9 +21,8 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		}
 		else
-			(h->str == next)
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 			h = h->next;
 			count++;
 		}
