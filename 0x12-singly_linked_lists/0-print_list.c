2309f3a21cfdb6246e16363324c6015365b98f35
jawd20 <jawd20@gmail.com>
2024-02-08 07:19:30 -0800

    number of nodes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,11 +5,11 @@
 
 /**
  * print_list - A function that returns the number of nodes
- * @h - the pointer that goes to the direction of the lis
- * Return - the number of nodes
+ * @h: the pointer that goes to the direction of the lis
+ * Return: the number of nodes
  */
 
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
